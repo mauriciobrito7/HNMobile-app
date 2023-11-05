81716e8ce35b38c0f1273b152272db87a73b9c44
Mauricio Brito <maubot01@gmail.com>
2023-11-05 11:19:11 -0300

    feat(useFetch): useFetch hook added

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,9 @@
 import { Stack } from 'expo-router';
 
 const Layout = () => {
-	return <Stack initialRouteName="home">
-		<Stack.Screen name="home" />
-	</Stack>;
+  return <Stack initialRouteName="home">
+    <Stack.Screen name="home" />
+  </Stack>;
 };
 
 export default Layout;
--- app/home.tsx
+++ app/home.tsx
@@ -1,19 +1,30 @@
+import useFetch from '@/hooks/useFetch';
+
 import { View, Text, Button } from 'react-native';
 import { useRouter } from 'expo-router';
 
 const MainScreen = () => {
-	const router = useRouter();
+  const router = useRouter();
+  const { data, isLoading } = useFetch(`${process.env.EXPO_PUBLIC_API_URL}search_by_date`, {
+    query: 'mobile',
+  });
 
-	const goToDetail = () => {
-		router.push('/home');
-	};
+  console.log('data', data);
 
-	return (
-		<View>
-			<Text>Main Screen</Text>
-			<Button title="Go to Detail" onPress={goToDetail} />
-		</View>
-	);
+  const goToDetail = () => {
+    router.push('/home');
+  };
+
+  if(isLoading) {
+    return <Text>loading...</Text>;
+  }
+
+  return (
+    <View>
+      <Text>Main Screen</Text>
+      <Button title="Go to Detail" onPress={goToDetail} />
+    </View>
+  );
 };
 
 export default MainScreen;
